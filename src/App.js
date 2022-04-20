f9a41a9a6f7d1587f19f2fb9e97efac9045c44d3
GlaudistonNeto <glaudistonson@gmail.com>
2022-04-20 17:52:33 -0300

    ✨feature: added_inline_css

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
       {/* component's CSS */}
       <MyComponents />
       <p>This is the paragraph of the App.JS</p>
+      {/* inline css */}
+      <p style={{ color: 'blue', padding: '25px', borderTop: '2px solid red' }}>
+        {/* inline css should be avoided */}
+        This element was styled with inline style
+      </p>
     </div>
   );
 }
