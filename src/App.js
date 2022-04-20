a90a61f90f6f98e6a00d7a4c601b152db930c783
GlaudistonNeto <glaudistonson@gmail.com>
2022-04-20 17:42:26 -0300

    ✨feature: added_component_css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponents from './components/MyComponents';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React CSS</h1>
+      {/* component's CSS */}
+      <MyComponents />
+      <p>This is the paragraph of the App.JS</p>
     </div>
   );
 }
---
+++ src/components/MyComponents.js
@@ -0,0 +1,13 @@
+import './MyComponents.css'
+
+const MyComponents = () => {
+  return (
+    <div>
+      <h1>Component's CSS</h1>
+      <p>This is the paragraph of the component</p>
+      <p className="my-comp-paragraph">This is also of the component</p>
+    </div>
+  );
+};
+
+export default MyComponents;
